64359ff8a40ef1e1d41074ed9f185273f3fff1a2
pwelch <paul.welch@templeandwebster.com.au>
2025-03-18 12:41:35 +1100

    Remove registerWith.

--- android/src/main/java/com/example/flutter_braintree/FlutterBraintreeDropIn.java
+++ android/src/main/java/com/example/flutter_braintree/FlutterBraintreeDropIn.java
@@ -8,7 +8,6 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.ActivityResultListener;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import android.app.Activity;
 import android.content.Intent;
--- android/src/main/java/com/example/flutter_braintree/FlutterBraintreePlugin.java
+++ android/src/main/java/com/example/flutter_braintree/FlutterBraintreePlugin.java
@@ -15,7 +15,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.plugin.common.PluginRegistry.ActivityResultListener;
 
 public class FlutterBraintreePlugin implements FlutterPlugin, ActivityAware, MethodCallHandler, ActivityResultListener {
